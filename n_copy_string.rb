9966af7c1f800f5260e5bf5fc12223fc4315feaa
sai_sudha <saisudhavadisina@gmail.com>
2021-04-28 16:58:21 +0530

    Prints the given string for n times where n is a non-negative integer

---
+++ n_copy_string.rb
@@ -0,0 +1,8 @@
+def multiple_string(str, n)
+return str*n
+end
+
+print multiple_string('saisudha',5),"\n"
+puts multiple_string('&',4),"\n"
+print multiple_string('$',2),"\n"
+print multiple_string('skr',3),"\n"
